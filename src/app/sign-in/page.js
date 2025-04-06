27af7e245cff80eba60627e110cf4ec09596e923
Thejaswi S <thejaswi4uns@gmail.com>
2025-04-07 02:08:49 +0530

    Added SignIn and SignOut

---
+++ src/app/sign-in/page.js
@@ -0,0 +1,10 @@
+// src/app/sign-in/page.js
+import { SignIn } from "@clerk/nextjs";
+
+export default function SignInPage() {
+  return (
+    <div>
+      <SignIn path="/sign-in" routing="path" />
+    </div>
+  );
+}
